5ce081ef75c6dadcadb346806ad4b95361222365
Macil Tamazirt <maciltam@gmail.com>
2026-01-03 11:06:40 +0100

    logs

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ const mockupRequest = {
 const processFunction = async ({ req, res, log }) => {
   log(typeof req);
   log(Object.keys(req));
+  log(req["bodyJson"]);
   const request = JSON.parse(req);
   const { tableData, files } = request.body;
   const { candidate1, candidate2 } = files;
