706ab28e28da6cecd21b3ae01506d9866c4014dc
Macil Tamazirt <maciltam@gmail.com>
2026-01-04 16:51:34 +0100

    added modularity to promises

--- main.js
+++ main.js
@@ -7,6 +7,37 @@ const testObject = {
   key3: "value3",
 };
 
+const composeUploads = async ({ candidate1, candidate2 }) => {
+  const photoBucket = process.env.BUCKET_ID;
+  const cvBucket = process.env.BUCKET_ID;
+  let c1 = { photo: null, cv: null };
+  let c2 = { photo: null, cv: null };
+  //Upload cv1 and photo1
+  const cv1Response = await uploadFile(candidate1.cv, cvBucket);
+  c1.cv = cv1Response.$id;
+  const photo1Response = await uploadFile(candidate1.photo, photoBucket);
+  c1.photo = photo1Response.$id;
+  if (candidate2.photo) {
+    //Upload cv2 and photo2
+    const cv2Response = await uploadFile(candidate2.cv, cvBucket);
+    c2.cv = cv2Response.$id;
+    const photo2Response = await uploadFile(candidate2.photo, photoBucket);
+    c2.photo = photo2Response.$id;
+  }
+  return {
+    c1,
+    c2,
+  };
+};
+
+const prepareRow = (receivedData, { c1, c2 }) => {
+  receivedData.candidate1_photo_id = c1.photo;
+  receivedData.candidate1_cv_id = c1.cv;
+  receivedData.candidate2_photo_id = c2.photo;
+  receivedData.candidate2_cv_id = c2.cv;
+  return receivedData;
+};
+
 const testString = JSON.stringify(testObject);
 
 const testBuffer = Buffer.from(testString, "utf-8");
@@ -38,82 +69,17 @@ const processFunction = async ({ req, res, log }) => {
   const { candidate1, candidate2 } = files;
   const response = { status: "failed" };
 
-  let applicationType = "simple";
-  if (candidate2.cv) {
-    applicationType = "double";
-  }
-
   let candidate1Ids = {};
   let candidate2Ids = {};
 
   const photoBucket = process.env.BUCKET_ID;
   const cvBucket = process.env.BUCKET_ID;
-  log(photoBucket);
-  log(cvBucket);
 
-  if (applicationType == "simple") {
-    uploadFile(candidate1.photo, photoBucket)
-      .then((response) => {
-        candidate1Ids.photo = response.$id;
-      })
-      .then(() => {
-        return uploadFile(candidate1.cv, cvBucket);
-      })
-      .then((response) => {
-        candidate1Ids.cv = response.$id;
-      });
-  } else if (applicationType == "double") {
-    await uploadFile(candidate1.photo, photoBucket)
-      .then((response) => {
-        console.log("Candidate 1 photo upload response:", response);
-        candidate1Ids.photo = response.$id;
-      })
-      .then(() => {
-        return uploadFile(candidate1.cv, cvBucket);
-      })
-      .then((response) => {
-        console.log("Candidate 1 CV upload response:", response);
-        candidate1Ids.cv = response.$id;
-      })
-      .then(() => {
-        return uploadFile(candidate2.cv, cvBucket);
-      })
-      .then((response) => {
-        console.log("Candidate 2 CV upload response:", response);
-        candidate2Ids.cv = response.$id;
-      })
-      .then(() => {
-        return uploadFile(candidate2.photo, photoBucket);
-      })
-      .then((response) => {
-        console.log("Candidate 2 photo upload response:", response);
-        candidate2Ids.photo = response.$id;
-      })
-      .then(() => {
-        return {
-          candidate1: candidate1Ids,
-          candidate2: candidate2Ids,
-        };
-      })
-      .then((ids) => {
-        tableData.candidate1_photo_id = ids.candidate1.photo;
-        tableData.candidate1_cv_id = ids.candidate1.cv;
-        tableData.candidate2_photo_id = ids.candidate2.photo;
-        tableData.candidate2_cv_id = ids.candidate2.cv;
-        return tableData;
-      })
-      .then((row) => {
-        const response = createRow(row);
-        return response;
-      })
-      .then(() => {
-        response = { status: "success" };
-      })
-      .catch(() => {
-        response = { status: "error" };
-      });
-  }
-  return res.text(JSON.stringify(response));
+  const { c1, c2 } = await composeUploads(candidate1, candidate2);
+  const row = prepareRow(tableData, { c1, c2 });
+  const uploadResponse = await createRow(row);
+
+  return res.text(JSON.stringify(uploadResponse));
 };
 
 module.exports = processFunction;
