aa2ff2d6355368a7be93c0317e34c200dddb6bc3
Macil Tamazirt <maciltam@gmail.com>
2026-01-09 16:22:43 +0100

    added a log

--- main.js
+++ main.js
@@ -54,7 +54,9 @@ const prepareRow = (receivedData, { c1, c2 }, application_type) => {
 };
 
 const processFunction = async ({ req, res, log }) => {
+  log("Started function");
   log(req);
+
   const { table_data, files, personal_code } = req.bodyJson;
 
   const { candidate1, candidate2 } = files;
